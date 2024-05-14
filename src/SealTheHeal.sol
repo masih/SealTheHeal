a4c1ddc4353e33a5dd6c27c1f9821fca509e936c
Masih H. Derkani <m@derkani.org>
2024-05-14 08:16:42 +0100

    Initial contract implementation

--- src/SealTheHeal.sol
+++ src/SealTheHeal.sol
@@ -1,142 +1,128 @@
 pragma solidity ^0.8.13;
 
-import {MarketAPI} from "../lib/filecoin-solidity/contracts/v0.8/MarketAPI.sol";
 import {CommonTypes} from "../lib/filecoin-solidity/contracts/v0.8/types/CommonTypes.sol";
+import {MarketAPI} from "../lib/filecoin-solidity/contracts/v0.8/MarketAPI.sol";
 import {MarketTypes} from "../lib/filecoin-solidity/contracts/v0.8/types/MarketTypes.sol";
-import {Actor} from "../lib/filecoin-solidity/contracts/v0.8/utils/Actor.sol";
-import {Misc} from "../lib/filecoin-solidity/contracts/v0.8/utils/Misc.sol";
+import {SendAPI} from "../lib/filecoin-solidity/contracts/v0.8/SendAPI.sol";
 
-/*
-abi encode calls using cast calldata "<stub>" [param,param...]
+interface SealHealer {
+    function setBounty(bytes calldata cid, uint256 size) external payable;
 
-example 
-cast calldata "addCID(bytes,uint256)" 000181e203922020c8a491097fa5272f2d7586685f3ea081e129bb820d6322248d67832c4f601d3c 128
-out
-0xd4a0cd0a000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000028000181e203922020c8a491097fa5272f2d7586685f3ea081e129bb820d6322248d67832c4f601d3c000000000000000000000000000000000000000000000000
-*/
+    function claimBounty(uint64 dealID) external;
 
-/* 
-Contract Usage
-    Step   |   Who   |    What is happening  |   Why 
-    ------------------------------------------------
-    Deploy | contract owner   | contract owner deploys address is owner who can call addCID  | create contract setting up rules to follow
-    AddCID | data pinners     | set up cids that the contract will incentivize in deals      | add request for a deal in the filecoin network, "store data" function
-    Fund   | contract funders |  add FIL to the contract to later by paid out by deal        | ensure the deal actually gets stored by providing funds for bounty hunter and (indirect) storage provider
-    Claim  | bounty hunter    | claim the incentive to complete the cycle                    | pay back the bounty hunter for doing work for the contract
+    function bountyByPiece(bytes memory cid) external view returns (uint256);
 
-*/
+    function dealByPiece(bytes memory cid) external view returns (uint64);
 
-// USERS BE AWARE: reward is given all at once and every time to the original SP and any repairer. 
+    function pieceBySize(bytes memory cid) external view returns (uint256);
+
+    function pieceByIndex(uint256 index) external view returns (bytes memory);
+
+    function pieceCount() external view returns (uint256);
+
+    function owner() external view returns (address);
+}
+
+// implement dutch auction.
+// https://en.wikipedia.org/wiki/Dutch_auction
+
+// PoC Disclaimer: reward is given all at once and every time to the original SP and any repairer.
 // Do not use this until the WTF above is rectified.
-contract SealTheHeal {
+contract SealTheHeal is SealHealer {
 
-    mapping(bytes => uint) public cidToPriceSet;
-    mapping(bytes => uint64) public cidToDealSet;
-    mapping(bytes => uint) public cidSizes;
+    int256 constant internal EX_DEAL_EXPIRED = 32;
+    int64 constant internal EPOCH_UNSPECIFIED = - 1;
+
+    mapping(bytes => uint64) internal maxClaimsByPiece;
+
+    mapping(bytes => uint256) public bountyByPiece;
+    mapping(bytes => uint64) public dealByPiece;
+    mapping(bytes => uint256) public pieceBySize;
+    bytes[] public pieceByIndex;
 
     address public owner;
-    address constant CALL_ACTOR_ID = 0xfe00000000000000000000000000000000000005;
-    uint64 constant DEFAULT_FLAG = 0x00000000;
-    uint64 constant METHOD_SEND = 0;
-    int256 constant EX_DEAL_EXPIRED = 32;
-    int64 constant UNSPECIFIED = - 1;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function fund(uint64 unused) public payable {}
+    function setBounty(bytes calldata cid, uint256 size, uint64 maxClaims) public payable {
+        require(msg.sender == owner, "only the contract owner can set bounty");
+        require(maxClaims > 0, "maximum number of claims must be larger than zero");
+        require(size > 0, "piece size must be larger than zero");
 
-    function addPieceCID(bytes calldata cidraw, uint size, uint reward) public {
-        require(msg.sender == owner);
-        require(reward > 0);
+        if (bountyByPiece[cid] == 0) {
+            // There is no previous bounty.
+            // * Require non-zero bounty, and
+            // * Set the bounty.
+            // * Update the list of pieces
+            require(msg.value > 0, "bounty must be more than zero");
+            bountyByPiece[cid] = msg.value;
+            pieceByIndex.push(cid);
+        } else if (msg.value > 0) {
+            // Override the bounty, because:
+            // * Bounty for piece already exists, and
+            // * message value is larger than zero.
+            bountyByPiece[cid] = msg.value;
+        }
 
-        cidToPriceSet[cidraw] = reward;
-        cidSizes[cidraw] = size;
+        pieceBySize[cid] = size;
+        maxClaimsByPiece[cid] = maxClaims;
     }
 
-    function hasDeal(bytes memory cidraw) internal view returns (bool) {
-        return cidToDealSet[cidraw] != 0;
+    function claimBounty(uint64 dealID) public {
+        (int256 exitCode, MarketTypes.GetDealDataCommitmentReturn memory commitment) =
+                            MarketAPI.getDealDataCommitment(dealID);
+
+        require(exitCode == 0, "cannot get deal data commitment");
+
+        bytes memory cid = commitment.data;
+        require(bountyByPiece[cid] > 0, "deal has no bounty");
+        require(pieceBySize[cid] == commitment.size, "deal piece size does not match");
+
+        if (dealByPiece[cid] == 0) {
+            // There is no pre-existing deal for piece.
+            // Check if dealID is active and if so give out the bounty 
+            require(isDealLive(dealID), "deal must be live before claiming bounty");
+        } else {
+            uint64 currentDealID = dealByPiece[cid];
+            require(isDealExpiredOrTerminated(currentDealID), "bounty already claimed");
+        }
+        reward(dealID, bountyByPiece[cid]);
+        dealByPiece[cid] = dealID;
     }
 
-    function authorizeDeal(bytes memory cidraw, uint size, uint64 deal_id) internal {
-        require(cidToPriceSet[cidraw] > 0, "cid must be added before authorizing");
-        require(cidSizes[cidraw] == size, "data size must match expected");
-        require(!hasDeal(cidraw), "deal failed policy check: cid has already been stored");
-        require(isDealLive(deal_id), "deal is not activated");
-
-        cidToDealSet[cidraw] = deal_id;
+    function pieceCount() external view returns (uint256) {
+        return pieceByIndex.length;
     }
 
-    function authorizeRepair(bytes memory cidraw, uint64 repair_deal_id) internal {
-        // Price has to be there because it is checked at the time of making deal by authorizeData.
-        require(hasDeal(cidraw), "deal failed policy check: cid has already been stored");
-
-        // TODO update comments below.
-        // Retrieve the state of previous deal for cidraw.
-        // Note that we only need to check the exit code from the actor call, because the termination epoch that may be
-        // returned  by GetDealActivationReturn is ephemeral. A deal is only deemed expired once the actor call explicitly
-        // exits with EX_DEAL_EXPIRED.
-        uint64 prev_deal_id = cidToDealSet[cidraw];
-        (int256 exit_code, MarketTypes.GetDealActivationReturn memory activation) = MarketAPI.getDealActivation(prev_deal_id);
-
-        // TODO comment about the or case.
-        require(exit_code == EX_DEAL_EXPIRED || CommonTypes.ChainEpoch.unwrap(activation.terminated) > UNSPECIFIED, "previous deal has not expired yet");
-        cidToDealSet[cidraw] = repair_deal_id;
-    }
-
-    function claim_bounty(uint64 deal_id) public {
-        (int256 exit_code, MarketTypes.GetDealDataCommitmentReturn memory commitmentRet) = MarketAPI.getDealDataCommitment(deal_id);
-        require(exit_code == 0, "cannot get deal data commitment");
-
-        authorizeDeal(commitmentRet.data, commitmentRet.size, deal_id);
-
-        // get dealer (bounty hunter client)
-        (int256 dc_exit_code, uint64 client_id) = MarketAPI.getDealClient(deal_id);
-        require(dc_exit_code == 0, "cannot get client ID");
-
-        // send reward to client 
-        send(client_id, cidToPriceSet[commitmentRet.data]);
-    }
-
-    function claim_repair(uint64 repair_deal_id) public {
-
-        // TODO make the UX smoother for the case where a CID has no deal but claim repair is called.
-
-        (int256 exit_code, MarketTypes.GetDealDataCommitmentReturn memory commitmentRet) = MarketAPI.getDealDataCommitment(repair_deal_id);
-        require(exit_code == 0, "cannot get deal data commitment");
-
-        authorizeRepair(commitmentRet.data, repair_deal_id);
-
-        // get dealer (bounty hunter client)
-        (int256 dc_exit_code, uint64 client_id) = MarketAPI.getDealClient(repair_deal_id);
-        require(dc_exit_code == 0, "cannot get client ID");
-
-        // TODO give proportional reward and parameterise repair reward proportion.
-        // TODO add public calls to check if a cid needs more funds for repair to be facilitated.
-        // send reward to client 
-        send(client_id, cidToPriceSet[commitmentRet.data]);
-    }
-
-    function isDealLive(uint64 deal_id) internal view returns (bool) {
-        (int256 exit_code, MarketTypes.GetDealActivationReturn memory activation) = MarketAPI.getDealActivation(deal_id);
+    function isDealLive(uint64 dealID) internal view returns (bool) {
+        (int256 exitCode, MarketTypes.GetDealActivationReturn memory activation) = MarketAPI.getDealActivation(dealID);
 
         // TODO update this comment
         // Check that the deal is activated. A deal is active when:
         // 1) the actor call exits with code 0, i.e. the deal is live, and
         // 2) activation epoch is not -1, i.e. there is an activation epoch.
         //
-        // Note that we do not need to check whether the termination epoch is set, because termination 
-        // epoch is irrelevant. Meaning, a deal is not expired until it is signalled explicitly by the 
+        // Note that we do not need to check whether the termination epoch is set, because termination
+        // epoch is irrelevant. Meaning, a deal is not expired until it is signalled explicitly by the
         // actor exit code.
-        return exit_code == 0 && CommonTypes.ChainEpoch.unwrap(activation.activated) != UNSPECIFIED && CommonTypes.ChainEpoch.unwrap(activation.terminated) == UNSPECIFIED;
+        return exitCode == 0 && CommonTypes.ChainEpoch.unwrap(activation.activated) != EPOCH_UNSPECIFIED
+            && CommonTypes.ChainEpoch.unwrap(activation.terminated) == EPOCH_UNSPECIFIED;
     }
 
-    // send reward to client, i.e. dealer.
-    function send(uint64 actorID, uint reward) internal {
-        bytes memory emptyParams = "";
-        delete emptyParams;
-        // TODO review if static_call value should be false.
-        Actor.callByID(CommonTypes.FilActorId.wrap(actorID), METHOD_SEND, Misc.NONE_CODEC, emptyParams, reward, false);
+    function isDealExpiredOrTerminated(uint64 dealID) internal view returns (bool) {
+        (int256 exitCode, MarketTypes.GetDealActivationReturn memory activation) = MarketAPI.getDealActivation(dealID);
+
+        // TODO comment about the "or" case.
+        return exitCode == EX_DEAL_EXPIRED || CommonTypes.ChainEpoch.unwrap(activation.terminated) > EPOCH_UNSPECIFIED;
+    }
+
+    function reward(uint64 dealID, uint256 amount) internal {
+        (int256 dcExitCode, uint64 clientID) = MarketAPI.getDealClient(dealID);
+        require(dcExitCode == 0, "failed to get deal client to send reward");
+
+        int256 sExitCode = SendAPI.send(CommonTypes.FilActorId.wrap(clientID), amount);
+        require(sExitCode == 0, "failed to send reward to deal client");
     }
 }
